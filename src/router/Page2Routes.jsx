46b79a25c14e9f7324f161ab1066b716d3db5358
Youuu <31610705+73-yy@users.noreply.github.com>
2022-04-16 21:34:10 +0900

    make queryParams

---
+++ src/router/Page2Routes.jsx
@@ -0,0 +1,9 @@
+import { Page2 } from "../Page2";
+import { UrlParamater } from "../UrlParamater";
+
+export const Page2Routes = [
+  { path: "/", exact: true, children: <Page2 /> },
+  { path: "/:id", exact: false, children: <UrlParamater /> }
+];
+
+// urlパラメータの場合はコロンを先頭に付与する。
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
-import { Page2 } from "../Page2";
 import { Page1Routes } from "./Page1Routes";
+import { Page2Routes } from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -32,9 +29,26 @@ export const Router = () => {
           );
         }}
       />
-      <Route path="/page2">
-        <Page2 />
-      </Route>
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => {
+          return (
+            <Switch>
+              {Page2Routes.map((route) => {
+                return (
+                  <Route
+                    key={route.path}
+                    exact={route.exact}
+                    path={`${url}${route.path}`}
+                  >
+                    {route.children}
+                  </Route>
+                );
+              })}
+            </Switch>
+          );
+        }}
+      />
     </Switch>
   );
 };
